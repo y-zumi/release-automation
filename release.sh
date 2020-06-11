6ee4a8ce02aa7ae21de5ebbdb566d3bf092ce1f1
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2020-06-12 02:44:49 +0900

    add(script): add current version message

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@
 # tag
 #!bin/bash
 echo "please write release version"
+echo "current release version: `hub release -L 1`"
 read NEXT_RELEASE_VER
 echo "$NEXT_RELEASE_VER is ok? y/N"; read
 if [ $REPLY = "y" ] ; then
